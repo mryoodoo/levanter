e8e5e2be19de3c35790f1c20ca6229b21c0111d2
mryoodoo <mryoodoo@gmail.com>
2025-05-01 12:55:44 -0400

    config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || 'levanter_1ae352f72fcf541cab27e445a8a752d45').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
